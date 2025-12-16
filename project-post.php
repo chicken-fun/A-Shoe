3fe143345ba81d9102d1efc6e5c074806a739ed4
chicken-fun <chickenandtheshoemaker@gmail.com>
2025-12-16 01:35:45 -0700

    php

---
+++ project-post.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+	body { borackground-color: #404040; col: white; line-height: 2; } 
+</style>
+</head>
+
+<body>
+
+
+
+first <?php echo $_POST["firstname"]; ?><br>
+last <?php echo $_POST["lastname"]; ?><br>
+email <?php echo $_POST["email"]; ?>
+comment <?php echo $_POST["comments"]; ?><br>
+
+</body>
+</html>
